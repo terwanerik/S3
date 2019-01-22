82ffa48ba07aa3fb5fe30f949af25022c8ff9453
Erik Terwan <erik.t@childhealthcaresolutions.com>
2019-01-22 13:38:49 +0100

    Updated package in hopes of working

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
@@ -6,12 +6,10 @@ let package = Package(
     products: [
         .library(name: "S3", targets: ["S3"]),
         .library(name: "S3Signer", targets: ["S3Signer"]),
-        .library(name: "S3TestTools", targets: ["S3TestTools"])
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/LiveUI/XMLCoding.git", from: "0.1.0"),
-        .package(url: "https://github.com/LiveUI/VaporTestTools.git", from: "0.1.5")
     ],
     targets: [
         .target(name: "S3", dependencies: [
@@ -34,15 +32,5 @@ let package = Package(
             "Vapor"
             ]
         ),
-        .target(name: "S3TestTools", dependencies: [
-            "Vapor",
-            "VaporTestTools",
-            "S3"
-            ]
-        ),
-        .testTarget(name: "S3Tests", dependencies: [
-            "S3"
-            ]
-        )
     ]
 )
